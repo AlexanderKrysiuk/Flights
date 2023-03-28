3b6fe8e814324c3fae6a80f219ed7b6d8441c35d
Hacker-Wolf <alex.krysiuk@icloud.com>
2023-03-28 22:53:02 +0200

    Creating and Routing a Custom Component

--- Flights/Flights/ClientApp/src/app/app.module.ts
+++ Flights/Flights/ClientApp/src/app/app.module.ts
@@ -6,18 +6,20 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { SearchFlightsComponent } from './search-flights/search-flights.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    SearchFlightsComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '', component: SearchFlightsComponent, pathMatch: 'full' },
     ])
   ],
   providers: [],
